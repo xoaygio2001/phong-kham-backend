2f764d573b6f09784bbf40cbfb59d1c2fc690084
xoaygio2001 <xoaygio2001@gmail.com>
2023-07-20 07:40:19 +0700

    first commit

---
+++ src/migrations/migration-create-booking.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('Bookings', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            statusId: {
+                type: Sequelize.STRING
+            },
+            doctorId: {
+                type: Sequelize.INTEGER
+            },
+            patientId: {
+                type: Sequelize.INTEGER
+            },
+            STT: {
+                type: Sequelize.STRING
+            },
+            date: {
+                type: Sequelize.STRING
+            },
+            timeType: {
+                type: Sequelize.STRING
+            },
+            token: {
+                type: Sequelize.STRING
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('Bookings');
+    }
+};
